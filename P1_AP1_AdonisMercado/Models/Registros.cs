7610e11d51304eaae04eb7eb55e5b13688b34a3e
adonismercado <adonis_mercado@ucne.edu.do>
2025-09-22 20:05:49 -0400

    Agregando la llave primaria RegistroId en el modelo Registros

--- P1_AP1_AdonisMercado/Models/Registros.cs
+++ P1_AP1_AdonisMercado/Models/Registros.cs
@@ -1,6 +1,10 @@
-﻿namespace P1_AP1_AdonisMercado.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace P1_AP1_AdonisMercado.Models;
 
 public class Registros
 {
+    [Key]
+    public int RegistroId { get; set; }
 }
 
