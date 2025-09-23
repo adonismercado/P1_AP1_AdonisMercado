27af4001f740434dce4d9f8d62483cb901f1731d
adonismercado <adonis_mercado@ucne.edu.do>
2025-09-22 20:12:42 -0400

    Creando el servicio RegistrosService con su metodo Listar

---
+++ P1_AP1_AdonisMercado/Services/RegistrosService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P1_AP1_AdonisMercado.DAL;
+using P1_AP1_AdonisMercado.Models;
+using System.Linq.Expressions;
+
+namespace P1_AP1_AdonisMercado.Services;
+
+public class RegistrosService(IDbContextFactory<Contexto> DbFactory)
+{
+    public async Task<List<Registros>> Listar(Expression<Func<Registros, bool>> criterio)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Registros
+            .Where(criterio)
+            .AsNoTracking()
+            .ToListAsync();
+    }
+}
+
