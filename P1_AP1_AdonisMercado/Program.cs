e4acfcd2f159350b4ae4da3158ce9e4fb1739a29
adonismercado <adonis_mercado@ucne.edu.do>
2025-09-22 20:24:57 -0400

    Inyectando el servicio RegistrosService en el programa

--- P1_AP1_AdonisMercado/Program.cs
+++ P1_AP1_AdonisMercado/Program.cs
@@ -1,6 +1,7 @@
 using P1_AP1_AdonisMercado.Components;
 using P1_AP1_AdonisMercado.DAL;
 using Microsoft.EntityFrameworkCore;
+using P1_AP1_AdonisMercado.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,7 +11,7 @@ builder.Services.AddRazorComponents()
 var ConStr = builder.Configuration.GetConnectionString("ConStr");
 builder.Services.AddDbContextFactory<Contexto>(o => o.UseSqlite(ConStr));
 
-
+builder.Services.AddScoped<RegistrosService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
