05db1927304794d61a62e3765af342d687d97477
adonismercado <adonis_mercado@ucne.edu.do>
2025-09-29 19:46:03 -0400

    Cambio en el mensaje de error de los atributos Cantidad y Precio en el modelo EntradasHuacales

--- P1_AP1_AdonisMercado/Models/EntradasHuacales.cs
+++ P1_AP1_AdonisMercado/Models/EntradasHuacales.cs
@@ -13,10 +13,10 @@ public class EntradasHuacales
     [Required(ErrorMessage = "Error: El nombre del cliente es obligatorio.")]
     public string NombreCliente { get; set; }
 
-    [Range(1, int.MaxValue, ErrorMessage = "Error: La cantidad es obligatoria.")]
+    [Range(1, int.MaxValue, ErrorMessage = "Error: La cantidad debe ser mayor a 1.")]
     public int Cantidad { get; set; }
 
-    [Range(1, double.MaxValue, ErrorMessage = "Error: El precio es obligatorio.")]
+    [Range(1, double.MaxValue, ErrorMessage = "Error: El precio debe ser mayor a 1.")]
     public decimal Precio { get; set; }
 }
 
