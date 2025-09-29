bca1ad2cd5a8cdb2b80aa2da196a5436cc1ed394
adonismercado <adonis_mercado@ucne.edu.do>
2025-09-29 19:44:25 -0400

    Creando una nueva migracion y una nueva base de datos para aplicar los cambios hechos a los atributos Cantidad y Precio en el modelo EntradasHuacales

---
+++ P1_AP1_AdonisMercado/Migrations/20250929234340_RangeToPrecioAndCantidad.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace P1_AP1_AdonisMercado.Migrations
+{
+    /// <inheritdoc />
+    public partial class RangeToPrecioAndCantidad : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
