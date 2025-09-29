c62a103f59987bf966b104d605cdddf36912b606
adonismercado <adonis_mercado@ucne.edu.do>
2025-09-29 18:18:56 -0400

    Agregando atributos al modelo EntradasHuacales

--- P1_AP1_AdonisMercado/Models/EntradasHuacales.cs
+++ P1_AP1_AdonisMercado/Models/EntradasHuacales.cs
@@ -3,9 +3,20 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace P1_AP1_AdonisMercado.Models;
 
 public class EntradasHuacales
-
 {
     [Key]
-    public int RegistroId { get; set; }
+    public int IdEntrada { get; set; }
+
+    [Required(ErrorMessage = "Error: La fecha es obligatoria.")]
+    public DateTime Fecha { get; set; } = DateTime.Now;\
+
+    [Required(ErrorMessage = "Error: El nombre del cliente es obligatorio.")]
+    public string NombreCliente { get; set; }
+
+    [Required(ErrorMessage = "Error: La cantidad es obligatoria.")]
+    public int Cantidad { get; set; }
+
+    [Required(ErrorMessage = "Error: El precio es obligatorio.")]
+    public int Precio { get; set; }
 }
 
