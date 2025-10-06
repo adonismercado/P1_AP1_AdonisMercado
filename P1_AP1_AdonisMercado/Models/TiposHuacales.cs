e5272c341061dbbe261178524362ec4a7794c367
adonismercado <adonis_mercado@ucne.edu.do>
2025-10-06 19:49:47 -0400

    Creando el modelo TiposHuacales

---
+++ P1_AP1_AdonisMercado/Models/TiposHuacales.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace P1_AP1_AdonisMercado.Models;
+
+public class TiposHuacales
+{
+    [Key]
+    public int TipoId { get; set; }
+
+    [Required(ErrorMessage = "Error: La descripcion es obligatoria.")]
+    public string Descripcion { get; set; }
+
+    public int Existencia { get; set; }
+
+}
