319a96009697a5e12980180e9e29db33ab20f922
adonismercado <adonis_mercado@ucne.edu.do>
2025-10-06 20:21:45 -0400

    Creando el OnModelCreating en el contexto

--- P1_AP1_AdonisMercado/DAL/Contexto.cs
+++ P1_AP1_AdonisMercado/DAL/Contexto.cs
@@ -7,5 +7,35 @@ public class Contexto : DbContext
     public Contexto(DbContextOptions<Contexto> options) : base(options) { }
 
     public DbSet<EntradasHuacales> EntradasHuacales { get; set; }
+    public DbSet<TiposHuacales> TiposHuacales { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TiposHuacales>().HasData(
+            new List<TiposHuacales>()
+            {
+                new()
+                {
+                    TipoId = 1,
+                    Descripcion = "Huacal Verde",
+                    Existencia = 0,
+                },
+                new()
+                {
+                    TipoId = 2,
+                    Descripcion = "Huacal Rojo",
+                    Existencia = 0,
+                },
+                new()
+                {
+                    TipoId = 3,
+                    Descripcion = "Huacal Negro",
+                    Existencia = 0,
+                }
+            }
+        );
+        base.OnModelCreating(modelBuilder);
+    }
+
 }
 
